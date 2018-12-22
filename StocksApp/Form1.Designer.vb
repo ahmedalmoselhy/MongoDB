fd7482a291c821aeef9970a7e51638696a788195
ahmedalmoselhy <ahmedalmoselhy.slm@gmail.com>
2018-12-22 15:15:30 +0200

    full project

---
+++ StocksApp/Form1.Designer.vb
@@ -0,0 +1,301 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim DataGridViewCellStyle1 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim DataGridViewCellStyle2 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim DataGridViewCellStyle3 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim DataGridViewCellStyle4 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox1.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AllowUserToAddRows = False
+        Me.DataGridView1.AllowUserToResizeColumns = False
+        Me.DataGridView1.AllowUserToResizeRows = False
+        DataGridViewCellStyle1.Font = New System.Drawing.Font("Tahoma", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        DataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.[True]
+        Me.DataGridView1.AlternatingRowsDefaultCellStyle = DataGridViewCellStyle1
+        Me.DataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill
+        Me.DataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None
+        DataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Control
+        DataGridViewCellStyle2.Font = New System.Drawing.Font("Tahoma", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        DataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.WindowText
+        DataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight
+        DataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText
+        DataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.[True]
+        Me.DataGridView1.ColumnHeadersDefaultCellStyle = DataGridViewCellStyle2
+        Me.DataGridView1.ColumnHeadersHeight = 30
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing
+        DataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft
+        DataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Window
+        DataGridViewCellStyle3.Font = New System.Drawing.Font("Tahoma", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        DataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.ControlText
+        DataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight
+        DataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText
+        DataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.[True]
+        Me.DataGridView1.DefaultCellStyle = DataGridViewCellStyle3
+        Me.DataGridView1.Location = New System.Drawing.Point(12, 12)
+        Me.DataGridView1.MultiSelect = False
+        Me.DataGridView1.Name = "DataGridView1"
+        DataGridViewCellStyle4.BackColor = System.Drawing.SystemColors.Control
+        DataGridViewCellStyle4.Font = New System.Drawing.Font("Tahoma", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        DataGridViewCellStyle4.ForeColor = System.Drawing.SystemColors.WindowText
+        DataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.Highlight
+        DataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.HighlightText
+        DataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.[True]
+        Me.DataGridView1.RowHeadersDefaultCellStyle = DataGridViewCellStyle4
+        Me.DataGridView1.RowHeadersVisible = False
+        Me.DataGridView1.RowHeadersWidth = 50
+        Me.DataGridView1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing
+        Me.DataGridView1.RowTemplate.Height = 120
+        Me.DataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
+        Me.DataGridView1.Size = New System.Drawing.Size(421, 468)
+        Me.DataGridView1.TabIndex = 1
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Button5)
+        Me.GroupBox1.Controls.Add(Me.Button1)
+        Me.GroupBox1.Controls.Add(Me.PictureBox1)
+        Me.GroupBox1.Controls.Add(Me.TextBox2)
+        Me.GroupBox1.Controls.Add(Me.TextBox1)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Font = New System.Drawing.Font("Tahoma", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox1.Location = New System.Drawing.Point(439, 29)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(422, 343)
+        Me.GroupBox1.TabIndex = 2
+        Me.GroupBox1.TabStop = False
+        '
+        'Button5
+        '
+        Me.Button5.Font = New System.Drawing.Font("Tahoma", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button5.Location = New System.Drawing.Point(317, 26)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(95, 30)
+        Me.Button5.TabIndex = 3
+        Me.Button5.Text = "بحث"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Tahoma", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(317, 96)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(95, 30)
+        Me.Button1.TabIndex = 6
+        Me.Button1.Text = "اختر الصورة"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.Color.Transparent
+        Me.PictureBox1.BackgroundImage = Global.StocksApp.My.Resources.Resources._Default
+        Me.PictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.PictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.PictureBox1.InitialImage = Nothing
+        Me.PictureBox1.Location = New System.Drawing.Point(106, 96)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(200, 200)
+        Me.PictureBox1.TabIndex = 5
+        Me.PictureBox1.TabStop = False
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Font = New System.Drawing.Font("Tahoma", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox2.Location = New System.Drawing.Point(106, 61)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(200, 21)
+        Me.TextBox2.TabIndex = 4
+        Me.TextBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Font = New System.Drawing.Font("Tahoma", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(106, 28)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(200, 21)
+        Me.TextBox1.TabIndex = 4
+        Me.TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Tahoma", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(17, 96)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(30, 13)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "صورة"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Tahoma", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(17, 64)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(54, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "نوع المنتج"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Tahoma", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(17, 31)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(56, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "رقم المنتج"
+        '
+        'Button8
+        '
+        Me.Button8.Enabled = False
+        Me.Button8.Font = New System.Drawing.Font("Tahoma", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button8.Location = New System.Drawing.Point(654, 414)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(220, 30)
+        Me.Button8.TabIndex = 5
+        Me.Button8.Text = "حفظ المنتج"
+        Me.Button8.UseVisualStyleBackColor = True
+        '
+        'Button7
+        '
+        Me.Button7.Font = New System.Drawing.Font("Tahoma", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button7.Location = New System.Drawing.Point(654, 378)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(220, 30)
+        Me.Button7.TabIndex = 5
+        Me.Button7.Text = "تعديل"
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("Tahoma", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(439, 450)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(209, 30)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "إضافة منتج جديد"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Font = New System.Drawing.Font("Tahoma", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.Location = New System.Drawing.Point(439, 414)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(209, 30)
+        Me.Button3.TabIndex = 3
+        Me.Button3.Text = "عرض الكل"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'Button4
+        '
+        Me.Button4.Font = New System.Drawing.Font("Tahoma", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button4.Location = New System.Drawing.Point(654, 450)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(220, 30)
+        Me.Button4.TabIndex = 3
+        Me.Button4.Text = "خروج"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Font = New System.Drawing.Font("Tahoma", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button6.Location = New System.Drawing.Point(439, 378)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(209, 30)
+        Me.Button6.TabIndex = 4
+        Me.Button6.Text = "إزالة المنتج"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.ClientSize = New System.Drawing.Size(886, 501)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.Button2)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Name = "Form1"
+        Me.Text = "Stocks App"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+End Class
---
+++ StocksApp/Form1.vb
@@ -0,0 +1,144 @@
+﻿Imports System
+Imports System.IO
+Imports System.Text
+Imports System.Drawing
+Imports System.Drawing.Imaging
+Imports System.Runtime.InteropServices
+Imports System.Drawing.Image
+Imports MongoDB.Bson
+Imports MongoDB.Bson.IO
+Imports MongoDB.Bson.Serialization
+Imports MongoDB.Driver
+Imports MongoDB.Driver.Linq
+Imports MongoDB.Driver.Builders
+Public Class Form1
+    Dim client As MongoClient
+    Dim db As IMongoDatabase
+    Dim collection As IMongoCollection(Of BsonDocument)
+    Dim dt As New DataTable
+    Dim flag As Boolean
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' connect to the database
+        client = New MongoClient("mongodb://localhost:27017/")
+        db = client.GetDatabase("db")
+        collection = db.GetCollection(Of BsonDocument)("Models")
+        dt.Columns.Add("id", Type.GetType("System.String"))
+        dt.Columns.Add("Model Type", Type.GetType("System.String"))
+        dt.Columns.Add("Image", Type.GetType("System.Byte[]"))
+    End Sub
+    ' add elements to the database
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim ms As MemoryStream = New MemoryStream()
+        PictureBox1.BackgroundImage.Save(ms, ImageFormat.Png)
+        Dim arr As Byte() = ms.ToArray()
+        Dim Models As BsonDocument = New BsonDocument
+        With Models
+            .Add("_id", TextBox1.Text)
+            .Add("_ModelType", TextBox2.Text)
+            .Add("_Image", arr)
+        End With
+        Try
+            collection.InsertOne(Models)
+        Catch ex As Exception
+            MsgBox("id is unique, choose a different one !")
+        End Try
+    End Sub
+    ' view all records in datagrid view
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click, Button4.Click, Button5.Click
+        If flag = True Then
+            DataGridView1.DataSource.clear()
+        End If
+        Dim filter = Builders(Of BsonDocument).Filter.Empty
+        For Each item As BsonDocument In collection.Find(filter).ToList
+            Dim id As BsonElement = item.GetElement("_id")
+            Dim ModelType As BsonElement = item.GetElement("_ModelType")
+            Dim image As BsonElement = item.GetElement("_Image")
+            Dim arr As Byte() = image.Value
+            Dim ms As MemoryStream = New MemoryStream(arr)
+            dt.Rows.Add(id.Value, ModelType.Value, ms.ToArray())
+        Next
+        DataGridView1.DataSource = dt
+        flag = True
+    End Sub
+    ' choose an image
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        OpenFileDialog1.Filter = "Image Files |*.bmp;*.gif;*.jpg;*.png;*.tif|Allfiles|*.*"
+        If OpenFileDialog1.ShowDialog() = DialogResult.OK Then
+            Dim ConvertedImg = New Bitmap(Image.FromFile(OpenFileDialog1.FileName, True), 120, 120)
+            PictureBox1.BackgroundImage = ConvertedImg
+        End If
+    End Sub
+    'exit
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        System.Environment.Exit(0)
+    End Sub
+    ' search for a specific record
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        Dim filter = Builders(Of BsonDocument).Filter.Empty
+        For Each item As BsonDocument In collection.Find(filter).ToList
+            Dim id As BsonElement = item.GetElement("_id")
+            Dim ModelType As BsonElement = item.GetElement("_ModelType")
+            Dim image As BsonElement = item.GetElement("_Image")
+            Dim arr As Byte() = image.Value
+            Dim ms As MemoryStream = New MemoryStream(arr)
+            If TextBox1.Text = id.Value Then
+                TextBox2.Text = ModelType.Value
+                PictureBox1.BackgroundImage = FromStream(ms)
+                Exit Sub
+            End If
+        Next
+        MsgBox("No Models found for this id !")
+        TextBox1.Clear()
+        TextBox2.Clear()
+        PictureBox1.BackgroundImage = My.Resources._Default
+    End Sub
+    Private Sub DataGridView1_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellClick
+        TextBox1.Text = DataGridView1.SelectedRows(0).Cells(0).Value.ToString
+        TextBox2.Text = DataGridView1.SelectedRows(0).Cells(1).Value.ToString
+        Dim arr As Byte() = DataGridView1.SelectedRows(0).Cells(2).Value
+        Dim ms As MemoryStream = New MemoryStream(arr)
+        PictureBox1.BackgroundImage = FromStream(ms)
+    End Sub
+    ' remove a record
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        Dim q = Builders(Of BsonDocument).Filter.Eq(Of String)("_id", DataGridView1.SelectedRows(0).Cells(0).Value.ToString)
+        collection.DeleteOne(q)
+        TextBox1.Clear()
+        TextBox2.Clear()
+        PictureBox1.BackgroundImage = My.Resources._Default
+        Button3.PerformClick()
+    End Sub
+    ' edit a record
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click, Button8.Click
+        TextBox1.Enabled = False
+        Button2.Enabled = False
+        Button5.Enabled = False
+        Button3.Enabled = False
+        Button6.Enabled = False
+        Button7.Enabled = False
+        Button8.Enabled = True
+
+    End Sub
+    ' save edited record
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        Dim fltr = Builders(Of BsonDocument).Filter.Eq(Of String)("_id", TextBox1.Text)
+        Dim ms As MemoryStream = New MemoryStream()
+        PictureBox1.BackgroundImage.Save(ms, ImageFormat.Png)
+        Dim arr As Byte() = ms.ToArray()
+        Dim Models As BsonDocument = New BsonDocument
+        With Models
+            .Add("_id", TextBox1.Text)
+            .Add("_ModelType", TextBox2.Text)
+            .Add("_Image", arr)
+        End With
+        collection.ReplaceOne(fltr, Models)
+        TextBox1.Enabled = True
+        Button2.Enabled = True
+        Button5.Enabled = True
+        Button3.Enabled = True
+        Button6.Enabled = True
+        Button7.Enabled = True
+        Button8.Enabled = False
+        Button3.PerformClick()
+    End Sub
+End Class
